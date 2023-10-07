1b33d57eed4540a1d6e3597d77265671902163a8
Afra Taiza <afrataiza@gmail.com>
2023-10-07 16:32:57 -0300

    feat: configura tailwindcss

--- checkpoint_fe3/tailwind.config.js
+++ checkpoint_fe3/tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'accent': "#1fb2a6",
+      },
+    },
+    
   },
   plugins: [],
-  darkMode: 'class',
-}
-
+  darkMode: "class",
+};

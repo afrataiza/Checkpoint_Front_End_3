f9a0ff8f504fb5b9de8ea4674cdffdba18bc38ff
Afra Taiza <afrataiza@gmail.com>
2023-10-07 20:01:19 -0300

    feat: atualiza header e adiciona estilização

--- checkpoint_fe3/src/components/DentistaCard/index.jsx
+++ checkpoint_fe3/src/components/DentistaCard/index.jsx
@@ -1,54 +1,23 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
-import axios from "axios";
- 
-const getDentistaImage = async () => {
-  try {
-    const response = await axios.get(
-      'https://api.unsplash.com/search/photos',
-      {
-        headers: {
-          Authorization: `Client-ID aBWzUYRz1o9GEYJM8FdDxXvSaNxTD_BgTue_QaWoKMI`,
-        },
-        params: {
-          query: 'professional dentist',
-          per_page: 50, 
-        },
-      }
-    );
+import { Link } from "react-router-dom";
+const DentistaCard = ({ dentist }) => {
 
-    return response.data.results.map((result) => result.urls.small);
-  } catch (error) {
-    console.error('Erro ao buscar imagens de médicos:', error);
-    return [];
+  if (!dentist || !dentist.nome) {
+    return <div>Dentista não encontrado.</div>;
   }
-};
-
-const DentistaCard = ({dentista}) => { 
-  const [dentistaImage, setDentistaImage] = useState(null);
-
-  useEffect(() => {
-    const fetchDentistaImage = async () => {
-      const dentistaImages = await getDentistaImage();
-      if (dentistaImages.length > 0) {
-        const randomIndex = Math.floor(Math.random() * dentistaImages.length);
-        setDentistaImage(dentistaImages[randomIndex]);
-      }
-    };
-
-    fetchDentistaImage();
-  }, []);
-
-    if(!dentista || !dentista.nome){
-        return <div>Dentista não encontrado.</div>
-    }
-    return(
-        <div>
-             <img src={dentistaImage}  alt={dentista.nome} />
-             <h2>{dentista.nome} {dentista.sobrenome}</h2>
-             <p>Nome de usuário: @{dentista.usuario.username}</p>
-        </div>
-    );
+  return (
+    <div className="bg-neutral-50 w-80 h-32 rounded-md flex flex-col gap-2 items-center shadow-md dark:bg-base-content p-2">
+      <h2 className="font-semibold text-xl">
+        {dentist.nome} {dentist.sobrenome}
+      </h2>
+      <p>Nome de usuário: @{dentist.usuario.username}</p>
+      <Link
+        to="/"
+        className="text-blue-500 hover:text-blue-700 hover:underline"
+      > Ver mais detalhes
+      </Link>
+    </div>
+  );
 };
 
 export default DentistaCard;

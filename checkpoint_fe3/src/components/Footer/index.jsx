afd53ca95a59eb48e224ce357f4579ba990c4cdf
Afra Taiza <afrataiza@gmail.com>
2023-10-07 20:00:24 -0300

    feat: atualiza e estiliza footer

--- checkpoint_fe3/src/components/Footer/index.jsx
+++ checkpoint_fe3/src/components/Footer/index.jsx
@@ -1,7 +1,23 @@
-export function Footer(){ 
-    return(
-       <div>
-        {/*<img src="/gmail.icon.svg" alt="" /> */} <h1>É um footer viu?</h1>
-       </div>
-    )
+import { BsInstagram, BsFacebook, BsGithub } from "react-icons/bs";
+
+export function Footer() {
+  return (
+    <div className="flex h-24 items-center bg-neutral-100 dark:bg-base-200 justify-around w-screen">
+      <div className="flex justify-center items-center flex-col gap-2 h-full">
+        <img src="/dentistFavIcon.png" alt="logo" className="w-12 h-12" />
+        <h2 className="text-accent font-bold text-xl">HAP ODONTO</h2>
+      </div>
+      <div className="flex gap-8">
+        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer" className="hover:text-accent-focus">
+          <BsInstagram size={30} />
+        </a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer" className="hover:text-accent-focus">
+          <BsFacebook size={30} />
+        </a>
+        <a href="https://github.com/afrataiza/Checkpoint_Front_End_3/" target="_blank" rel="noreferrer" className="hover:text-accent-focus">
+          <BsGithub size={30} />
+        </a>
+      </div>
+    </div>
+  );
 }

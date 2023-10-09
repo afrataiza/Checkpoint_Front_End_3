d3bfc60160adb8a4eed545a652e5cf6c88a2cceb
Afra Taiza <afrataiza@gmail.com>
2023-10-08 22:54:22 -0300

    feat: add componente toast

--- checkpoint_fe3/src/App.jsx
+++ checkpoint_fe3/src/App.jsx
@@ -4,12 +4,14 @@ import RoutesApp from "./routes";
 import { AuthProvider } from "./contexts/Auth";
 import { DefaultProvider } from "./contexts/DefaultContext";
 import { Footer }  from "./components/Footer";
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
     <div className="dark:bg-base-100 text-neutral-700 dark:text-neutral-200 overflow-x-hidden">    
       <AuthProvider>
         <DefaultProvider>
+          <Toaster />
           <RoutesApp />
           <Footer />
         </DefaultProvider>
